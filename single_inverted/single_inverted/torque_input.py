a9cacb7840404aae9e20cb9fcd12b2c973637106
priyansh3103 <152431356+priyansh3103@users.noreply.github.com>
2024-06-07 12:22:16 +0000

    Update torque_input.py

--- single_inverted/single_inverted/torque_input.py
+++ single_inverted/single_inverted/torque_input.py
@@ -27,18 +27,34 @@ class torque_input(Node):
             x = (np.pi - state.theta)
         if state.theta <0:
             x = -(np.pi + state.theta)
+
+        if state.theta > (np.pi/2) or state.theta < -(np.pi/2): #np.pi-0.53 is the angle after which torque by gravity < 5
+            error = x
+            p = 50
+            i = 1
+            d = 1.5
+            dt = 1/500
+            self.integral = self.integral + x*dt
+            self.derivative = (error - self.prev_error)/dt
+            torque.torque_value = (p*error + i*self.integral + d*self.derivative)
+            if torque.torque_value >5:
+                torque.torque_value = 5.0
+            if torque.torque_value <-5:
+                torque.torque_value = -5.0
+            self.torque_publisher.publish(torque)
+            self.prev_error = error
+                
+        if state.theta <= (np.pi/2) and state.theta >= -(np.pi/2):
+            if state.theta_dot >= 0:
+                torque.torque_value = 3.0
+            if state.theta_dot<0:
+                torque.torque_value = -3.0
             
-        error = x
-        p = 50
-        i = 1
-        d = 0.4
-        dt = 1/500
-        self.integral = self.integral + x*dt
-        self.derivative = (error - self.prev_error)/dt
-        torque.torque_value = (p*error + i*self.integral + d*self.derivative) 
-        self.torque_publisher.publish(torque)
-        self.prev_error = error
-        self.get_logger().info(f"error:{x} current_w:{state.theta_dot} net_torque:{torque.torque_value }")
+        
+            self.torque_publisher.publish(torque)
+        
+        
+        self.get_logger().info(f"error:{x} current_w:{state.theta_dot} net_torque:{torque.torque_value}")
         
         
 def main(args = None):
